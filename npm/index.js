1144f76cb6e717c70276279561a5e3cda68f5ce5
Marco Segreto <marco.segreto@gsa.gov>
2017-01-25 15:55:06 -0800

    Gave npm init scaffold a try
    Would like feedback on structure, did I put things in right place?
    Is it better to prompt the user in case they don't want front end
    deps at all?
    
    I think keeping all the scaffolding folders in the base of the repo
    will get confusing, although might be fine if all this repo has
    is generators.

---
+++ npm/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var Generator = require('yeoman-generator');
+
+module.exports = Generator.extend({
+  prompting: function() {
+    return this.prompt([{
+      type    : 'confirm',
+      name    : 'frontendDeps',
+      message : 'Will this project have front end libraries/dependencies',
+      default : true
+    }]).then((answers) => {
+      this.frontendDeps = answers.frontendDeps;
+    });
+  },
+
+  configuring: function() {
+    if (this.frontendDeps) {
+      this.log('Configuring npm now...');
+      this.log('\n');
+      this.composeWith(require.resolve('generator-npm-init/app'), {
+        repo: 'https://github.com/18F/<%= destFolderName %>',
+        author: '18f',
+        license: 'CC0-1.0'
+      });
+    }
+  }
+});
