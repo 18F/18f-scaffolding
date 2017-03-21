2420a55c18025e3e2858aae31fbfd1859a36a469
CM Lubinski <cm.lubinski@gmail.com>
2017-03-21 13:28:56 +0000

    Lint license, npm, readme, todo scaffolds.

--- npm/index.js
+++ npm/index.js
@@ -1,30 +1,30 @@
-'use strict';
-
 const Generator = require('yeoman-generator');
 const sharedConfig = require('../shared-config');
 
-module.exports = Generator.extend({
-  prompting: function() {
-    return this.prompt([{
-      type    : 'confirm',
-      name    : 'frontendDeps',
-      message : 'Will this project have front end libraries/dependencies',
-      default : true
-    }]).then((answers) => {
+const frontendDepsPrompt = {
+  type: 'confirm',
+  name: 'frontendDeps',
+  message: 'Will this project have front end libraries/dependencies',
+  default: true,
+};
+
+module.exports = class extends Generator {
+  prompting() {
+    return this.prompt([frontendDepsPrompt]).then((answers) => {
       this.config.set('frontendDeps', answers.frontendDeps);
     });
-  },
+  }
 
-  configuring: function() {
+  configuring() {
     if (this.config.get('frontendDeps')) {
       this.log('Configuring npm now...');
       this.composeWith(require.resolve('generator-npm-init/app'), {
         name: this.config.get('repoName'),
         description: this.config.get('description'),
-        repo: 'https://github.com/18F/' + this.config.get('repoName'),
+        repo: `https://github.com/18F/${this.config.get('repoName')}`,
         author: '18f',
-        license: sharedConfig.licenseShortName
+        license: sharedConfig.licenseShortName,
       });
     }
   }
-});
+};
