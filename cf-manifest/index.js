15432324f377d7bcd7ea20a801e5e1c1568fe29b
CM Lubinski <cm.lubinski@gmail.com>
2017-03-21 13:09:38 +0000

    Lint app and cf-manifest.

--- cf-manifest/index.js
+++ cf-manifest/index.js
@@ -1,30 +1,28 @@
-'use strict';
 const Generator = require('yeoman-generator');
 const sharedConfig = require('../shared-config');
 const todo = require('../todo');
 
-module.exports = Generator.extend({
-  prompting: function () {
+module.exports = class extends Generator {
+  prompting() {
     return this.prompt(
       sharedConfig.promptsFor(this.config,
                               sharedConfig.primaryLanguagePrompt,
-                              sharedConfig.runCommandPrompt)
-    ).then(
+                              sharedConfig.runCommandPrompt))
+      .then(
       sharedConfig.saveResultsTo(this.config,
                                  sharedConfig.primaryLanguagePrompt,
-                                 sharedConfig.runCommandPrompt)
-    );
-  },
-  writing: function () {
+                                 sharedConfig.runCommandPrompt));
+  }
+
+  writing() {
     if (this.config.get('runCommand') === sharedConfig.runCommandPrompt.default) {
       todo.add(this.config, this.fs, {
-        'Cloud.gov Manifests': ['Procfile\'s "run" command']
+        'Cloud.gov Manifests': ['Procfile\'s "run" command'],
       });
     }
     this.fs.copyTpl(
-        this.templatePath(this.config.get('primaryLanguage') + '/*'),
+        this.templatePath(`${this.config.get('primaryLanguage')}/*`),
         this.destinationPath(),
-        {runCommand: this.config.get('runCommand')}
-    );
+        this.config.getAll());
   }
-});
+};
