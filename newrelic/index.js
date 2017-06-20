27e06ef9bedeff3dcfbff2efc050dbbd5284aeff
Eric Schles <ericschles@gmail.com>
2017-06-20 16:38:35 -0400

    updating newrelic commit to include writing to manifest-dev.yml

--- newrelic/index.js
+++ newrelic/index.js
@@ -56,10 +56,42 @@ module.exports = class extends Generator {
         'https://raw.githubusercontent.com/EricSchles/new-relic/master/python-low-security.ini')
       .then((response) => {
         // Not EJS style, so we'll just search-and-replace
+        // generate the newrelic.ini file
         const content = response.data.replace(
             '[Project Name]', this.config.get('projectFullName'));
         this.fs.write(this.destinationPath('newrelic.ini'), content);
+        // update the manifest_dev.yml file
+        var cwd = process.cwd();
+        var manifest_dev = 'manifest_dev.yml';
+        if (fs.existsSync(manifest_dev)) {
+          var manifest_dev_body = this.readFileAsString(manifest_dev);
+          if (manifest_dev_body.indexOf('applications:') < 0) {
+            manifest_dev_body += '\napplications:\n';
+          if (manifest_dev_body.indexOf('env:') < 0) {
+            manifest_dev_body += '\n  env:\n';
+          }
+        }
+        if (manifest_dev_body.indexOf("NEW_RELIC_APP_NAME:") < 0) {
+          manifest_dev_body += '\n\tNEW_RELIC_APP_NAME: ' + this.projectFullName + ' (dev)';
+        }
+        if (manifest_dev_body.indexOf("NEW_RELIC_CONFIG_FILE:") < 0) {
+          manifest_dev_body += '\n\tNEW_RELIC_CONFIG_FILE: newrelic.ini';
+        }
+        if (manifest_dev_body.indexOf("NEW_RELIC_ENV:") < 0) {
+          manifest_dev_body += '\n\tNEW_RELIC_ENV: "dev"';
+        }
+        if (manifest_dev_body.indexOf("NEW_RELIC_LOG:") < 0) {
+          manifest_dev_body += '\n\t NEW_RELIC_LOG: "stdout"';
+        }
+        fs.writeFile(manifest_dev, manifest_dev_body, function (err) {
+          if (err) throw err;
+        });
+      } else {
+        this.log("Please run yo 18f:cf-manifest first");
+      }
       }).catch(this.env.error.bind(this.env));
   }
 }
+
+
 };
