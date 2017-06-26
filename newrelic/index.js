6d07e2eeaab19f7d23a3d34c89a690bf4e008c4d
Eric Schles <ericschles@gmail.com>
2017-06-26 16:39:41 -0400

    updating so travis will run

--- newrelic/index.js
+++ newrelic/index.js
@@ -3,7 +3,6 @@ const Generator = require('yeoman-generator');
 const fs = require('fs');
 
 module.exports = class extends Generator {
-
   constructor(args, opts) {
     // Calling the super constructor is important so our generator is correctly set up
     super(args, opts);
